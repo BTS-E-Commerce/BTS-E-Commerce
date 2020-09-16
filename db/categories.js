c3b11a645ed350c566d9610f5221a44955b51af4
Tyler Flood <tyler.flood1@yahoo.com>
2020-09-16 17:37:10 -0400

    createCategories db method.

---
+++ db/categories.js
@@ -0,0 +1,72 @@
+//~~~~~~~~~~~~~~~~~~~
+//~~~~~ IMPORTS ~~~~~
+//~~~~~~~~~~~~~~~~~~~
+
+const { client } = require("./index.js");
+
+//~~~~~~~~~~~~~~~~~~~
+//~~~~ FUNCTIONS ~~~~
+//~~~~~~~~~~~~~~~~~~~
+
+//-- Get Functions --
+//# Gets all categories from exotic-db
+async function getAllCategories() {
+  try {
+    const { rows: categories } = await client.query(`
+        SELECT *
+        FROM categories
+        `);
+
+    return categories;
+  } catch (error) {
+    throw error;
+  }
+}
+
+//-- Create Functions
+//# Creates a new category
+async function createCategories(categoryList) {
+  if (categoryList.length === 0) {
+    return;
+  }
+  const valuesStringInsert = categoryList
+    .map((_, index) => `$${index + 1}`)
+    .join("), (");
+
+  const valuesStringSelect = categoryList
+    .map((_, index) => `$${index + 1}`)
+    .join(", ");
+
+  try {
+    await client.query(
+      `
+    INSERT INTO categories(name)
+    VALUES (${valuesStringInsert})
+    ON CONFLICT (name) DO NOTHING;
+    `,
+      categoryList
+    );
+
+    const { rows } = await client.query(
+      `
+    SELECT * FROM categories
+    WHERE name
+    IN (${valuesStringSelect})
+    
+    `,
+      categoryList
+    );
+
+    return rows;
+  } catch (error) {
+    throw error;
+  }
+}
+
+//~~~~~~~~~~~~~~~~~~~
+//~~~~~ EXPORTS ~~~~~
+//~~~~~~~~~~~~~~~~~~~
+module.exports = {
+  getAllCategories,
+  createCategories,
+};
