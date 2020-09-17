3ef43e3dcca38ac378003d4c3b2e0d76a6fcb526
Samatha-Fae Rapier <samathafae.rapier@gmail.com>
2020-09-17 16:38:57 -0400

    Tested the Get Calls and Create Call to products table

--- db/init_db.js
+++ db/init_db.js
@@ -3,12 +3,18 @@
 //~~~~~~~~~~~~~~~~~~~
 
 // -- Client --
-const { client } = require('./client');
-const { getAllUsers, } = require('./index');
-const { createUser } = require('./users');
+const { client } = require("./client");
+const { getAllUsers } = require("./index");
+const { createUser } = require("./users");
+const {
+  createProduct,
+  getAllProducts,
+  getProductById,
+  getProductByName,
+  getProductsBySale,
+} = require("./products");
 
 //-- Database Imports --
-
 
 //~~~~~~~~~~~~~~~~~~~
 //~~~~ FUNCTIONS ~~~~
@@ -30,7 +36,6 @@ async function initializeTables() {
     await dropTables();
 
     await buildTables();
-
   } catch (error) {
     throw error;
   }
@@ -39,7 +44,7 @@ async function initializeTables() {
 //* Drops tables in order of dependencies. Must be used before building tables again.
 async function dropTables() {
   try {
-    console.log('Starting to drop tables...');
+    console.log("Starting to drop tables...");
 
     await client.query(`
       DROP TABLE IF EXISTS product_categories;
@@ -51,9 +56,9 @@ async function dropTables() {
       DROP TABLE IF EXISTS users;
     `);
 
-    console.log('Finished dropping tables!');
+    console.log("Finished dropping tables!");
   } catch (error) {
-    console.log('Error dropping tables.');
+    console.log("Error dropping tables.");
     throw error;
   }
 }
@@ -61,11 +66,11 @@ async function dropTables() {
 //* Builds tables to construct database. Must happen after dropping the tables.
 async function buildTables() {
   try {
-    console.log('Starting to build tables...');
+    console.log("Starting to build tables...");
     //# Should we split these up? This is a really long function, but we won't be doing this anywhere else, so maybe it's okay?
 
     //# Create users table.
-    console.log('Starting to create users tables...');
+    console.log("Starting to create users tables...");
     await client.query(`
       CREATE TABLE users (
         id SERIAL PRIMARY KEY,
@@ -74,10 +79,10 @@ async function buildTables() {
         admin BOOLEAN DEFAULT false
       );
     `);
-    console.log('Sucessfully finished building users table!');
+    console.log("Sucessfully finished building users table!");
 
     //# Create products table.
-    console.log('Starting to create products tables...');
+    console.log("Starting to create products tables...");
     await client.query(`
       CREATE TABLE products (
         id SERIAL PRIMARY KEY,
@@ -91,20 +96,20 @@ async function buildTables() {
         date DATE DEFAULT now()
       );
     `);
-    console.log('Sucessfully finished building products table!');
+    console.log("Sucessfully finished building products table!");
 
     //# Create categories table.
-    console.log('Starting to create categories tables...');
+    console.log("Starting to create categories tables...");
     await client.query(`
       CREATE TABLE categories (
         id SERIAL PRIMARY KEY,
         name VARCHAR(255) NOT NULL
       );
     `);
-    console.log('Sucessfully finished building categories table!');
+    console.log("Sucessfully finished building categories table!");
 
     //# Create product_categories table. Used for linking a product with the categories associated with it.
-    console.log('Starting to create product_categories tables...');
+    console.log("Starting to create product_categories tables...");
     await client.query(`
       CREATE TABLE product_categories (
         id SERIAL PRIMARY KEY,
@@ -113,10 +118,10 @@ async function buildTables() {
         UNIQUE ("productId", "categoryId")
       );
     `);
-    console.log('Sucessfully finished building categories table!');
+    console.log("Sucessfully finished building categories table!");
 
     //# Create orders table.
-    console.log('Starting to create orders tables...');
+    console.log("Starting to create orders tables...");
     await client.query(`
       CREATE TABLE orders (
         id SERIAL PRIMARY KEY,
@@ -124,10 +129,10 @@ async function buildTables() {
         "orderPrice" INTEGER NOT NULL
       );
     `);
-    console.log('Sucessfully finished building orders table!');
+    console.log("Sucessfully finished building orders table!");
 
     //# Create order_products table. Used for linking a users order with the products inside of the order.
-    console.log('Starting to create order_products tables...');
+    console.log("Starting to create order_products tables...");
     await client.query(`
       CREATE TABLE order_products (
         id SERIAL PRIMARY KEY,
@@ -138,10 +143,10 @@ async function buildTables() {
         UNIQUE ("orderId", "productId")
       );
     `);
-    console.log('Sucessfully finished building order_products table!');
+    console.log("Sucessfully finished building order_products table!");
 
     //# Create reviews table.
-    console.log('Starting to create reviews tables...');
+    console.log("Starting to create reviews tables...");
     await client.query(`
       CREATE TABLE reviews (
         id SERIAL PRIMARY KEY,
@@ -153,11 +158,11 @@ async function buildTables() {
         UNIQUE ("productId", "userId")
       );
     `);
-    console.log('Sucessfully finished building reviews table!');
+    console.log("Sucessfully finished building reviews table!");
 
-    console.log('Sucessfully finished building tables!');
+    console.log("Sucessfully finished building tables!");
   } catch (error) {
-    console.log('Error building tables.');
+    console.log("Error building tables.");
     throw error;
   }
 }
@@ -170,8 +175,6 @@ async function initializeData() {
     await initializeCategories();
     await initializeOrders();
     await initializeReviews();
-
-
   } catch (error) {
     throw error;
   }
@@ -180,47 +183,78 @@ async function initializeData() {
 //* Initializes the starting/default users.
 async function initializeUsers() {
   try {
-    const userOne = await createUser({ username: "brody", password: "password" });
+    const userOne = await createUser({
+      username: "brody",
+      password: "password",
+    });
     console.log(userOne);
-  } catch (error) {
-
-  }
+  } catch (error) {}
 }
 
 //* Initializes the starting/default products. This may pull from some sort of api??
 async function initializeProducts() {
   try {
+    console.log("Creating initial products...");
+    await createProduct({
+      name: "Baked Mac N Cheese",
+      description: "Grammys special Thanksgiving Baked Mac N Cheese",
+      image:
+        "https://www.recipetineats.com/wp-content/uploads/2018/05/Baked-Mac-and-Cheese_2.jpg",
+      inventory: "10",
+      basePrice: "7",
+      currentPrice: "5",
+      sale: true,
+      date: "09/15/20",
+    });
 
+    await createProduct({
+      name: "Mac N Cheese Breadsticks",
+      description:
+        "It's not an official movie night at Grammys without the Mac Sticks!",
+      image:
+        "https://img.buzzfeed.com/video-api-prod/assets/5643fc670b714aefadf31991ed2f0f2f/BFV11184_MacnCheeseBreadsticks-Thumb1080SQ.jpg?resize=300:*&output-format=webp&output-quality=auto",
+      inventory: "20",
+      basePrice: "5",
+      currentPrice: "3",
+      sale: true,
+      date: "09/17/20",
+    });
+
+    await createProduct({
+      name: "Vegan Mac N Cheese",
+      description: "Grammys special Mac for Auntie Sarah",
+      image:
+        "https://img.buzzfeed.com/video-api-prod/assets/d9fd07cb667d47288c03a873c76a3445/FB_1.jpg?resize=300:*&output-format=webp&output-quality=auto",
+      inventory: "5",
+      basePrice: "10",
+      currentPrice: "10",
+      sale: false,
+      date: "09/10/2020",
+    });
+
+    console.log("Finished making products...");
   } catch (error) {
-
+    console.log("Error creating initial products...");
+    throw error;
   }
 }
 
 //* Initializes the starting/default categories.
 async function initializeCategories() {
   try {
-
-  } catch (error) {
-
-  }
+  } catch (error) {}
 }
 
 //* Initializes the starting/default orders.
 async function initializeOrders() {
   try {
-
-  } catch (error) {
-
-  }
+  } catch (error) {}
 }
 
 //* Initializes the starting/default reviews.
 async function initializeReviews() {
   try {
-
-  } catch (error) {
-
-  }
+  } catch (error) {}
 }
 
 // -- Database Function Testing --
@@ -230,6 +264,7 @@ async function testDatabase() {
     console.log("Running tests of database functions...");
 
     await testUserFunctions();
+    await testProductFunctions();
 
     console.log("Successfully finished running tests of database functions!");
   } catch (error) {
@@ -241,10 +276,40 @@ async function testDatabase() {
 //# Tests the functions associated with the users table.
 async function testUserFunctions() {
   try {
-    console.log("Testing getAllUsers...")
+    console.log("Testing getAllUsers...");
     const allUsers = await getAllUsers();
-    console.log("Successfully ran getAllUsers: \n", allUsers);
+    console.log("Successfully ran getAllUsers:", allUsers);
   } catch (error) {
+    throw error;
+  }
+}
+
+//# Tests the functions for the products table.
+async function testProductFunctions() {
+  try {
+    console.log("Products Table Testing:");
+
+    console.log("Testing getAllProducts...");
+    const products = await getAllProducts();
+    console.log("Successfully tested getAllProducts:", products);
+
+    console.log("Testing getProductsById using product:", products[1].id);
+    const productById = await getProductById(products[1].id);
+    console.log("Successfully tested getProdctsById:", productById);
+
+    console.log("Testing getProductByName using product:", products[2].id);
+    const productByName = await getProductByName({
+      name: "Vegan Mac N Cheese",
+    });
+    console.log("Successfully tested getProductByName:", productByName);
+
+    console.log("Testing getProductsBySale...");
+    const productsBySale = await getProductsBySale();
+    console.log("Successfully tested getProductsBySale:", productsBySale);
+
+    console.log("Products Table Testing Completed!");
+  } catch (error) {
+    console.log("Error running tests on products...");
     throw error;
   }
 }
