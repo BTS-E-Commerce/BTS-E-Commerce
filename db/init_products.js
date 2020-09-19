5676afad284dac2cf01fffc6beb2fc0f64320f40
Tyler Flood <tyler.flood1@yahoo.com>
2020-09-19 12:28:19 -0400

    fixed the error by adding one delete query to deleteProduct

--- db/init_products.js
+++ db/init_products.js
@@ -9,9 +9,9 @@ const {
   getProductsBySale,
   updateProduct,
   deleteProduct,
-} = require("./index");
+} = require('./index');
 // -- Client --
-const { client } = require("./client");
+const { client } = require('./client');
 
 //~~~~~~~~~~~~~~~~~~~
 //~~~~ FUNCTIONS ~~~~
@@ -20,47 +20,47 @@ const { client } = require("./client");
 async function initializeProducts() {
   try {
     await createProduct({
-      name: "Baked Mac N Cheese",
-      description: "Grammys special Thanksgiving Baked Mac N Cheese",
+      name: 'Baked Mac N Cheese',
+      description: 'Grammys special Thanksgiving Baked Mac N Cheese',
       image:
-        "https://www.recipetineats.com/wp-content/uploads/2018/05/Baked-Mac-and-Cheese_2.jpg",
-      inventory: "10",
-      basePrice: "7",
-      currentPrice: "5",
+        'https://www.recipetineats.com/wp-content/uploads/2018/05/Baked-Mac-and-Cheese_2.jpg',
+      inventory: '10',
+      basePrice: '7',
+      currentPrice: '5',
       sale: true,
-      date: "09/15/20",
-      categories: ["baked", "oven"],
+      date: '09/15/20',
+      categories: ['baked', 'oven'],
     });
 
     await createProduct({
-      name: "Mac N Cheese Breadsticks",
+      name: 'Mac N Cheese Breadsticks',
       description:
         "It's not an official movie night at Grammys without the Mac Sticks!",
       image:
-        "https://img.buzzfeed.com/video-api-prod/assets/5643fc670b714aefadf31991ed2f0f2f/BFV11184_MacnCheeseBreadsticks-Thumb1080SQ.jpg?resize=300:*&output-format=webp&output-quality=auto",
-      inventory: "20",
-      basePrice: "5",
-      currentPrice: "3",
+        'https://img.buzzfeed.com/video-api-prod/assets/5643fc670b714aefadf31991ed2f0f2f/BFV11184_MacnCheeseBreadsticks-Thumb1080SQ.jpg?resize=300:*&output-format=webp&output-quality=auto',
+      inventory: '20',
+      basePrice: '5',
+      currentPrice: '3',
       sale: true,
-      date: "09/17/20",
-      categories: ["crunchy", "cheesy", "cheddar"],
+      date: '09/17/20',
+      categories: ['crunchy', 'cheesy', 'cheddar'],
     });
 
     await createProduct({
-      name: "Vegan Mac N Cheese",
-      description: "Grammys special Mac for Auntie Sarah",
+      name: 'Vegan Mac N Cheese',
+      description: 'Grammys special Mac for Auntie Sarah',
       image:
-        "https://img.buzzfeed.com/video-api-prod/assets/d9fd07cb667d47288c03a873c76a3445/FB_1.jpg?resize=300:*&output-format=webp&output-quality=auto",
-      inventory: "5",
-      basePrice: "10",
-      currentPrice: "10",
+        'https://img.buzzfeed.com/video-api-prod/assets/d9fd07cb667d47288c03a873c76a3445/FB_1.jpg?resize=300:*&output-format=webp&output-quality=auto',
+      inventory: '5',
+      basePrice: '10',
+      currentPrice: '10',
       sale: false,
-      date: "09/10/2020",
-      categories: ["vegan", "special", "cheese"],
+      date: '09/10/2020',
+      categories: ['vegan', 'special', 'cheese'],
     });
-    console.log("Finished making products...");
+    console.log('Finished making products...');
   } catch (error) {
-    console.log("Error creating initial products...");
+    console.log('Error creating initial products...');
     throw error;
   }
 }
@@ -68,48 +68,48 @@ async function initializeProducts() {
 //# Tests the functions for the products table.
 async function testProductFunctions() {
   try {
-    console.log("Products Table Testing:");
+    console.log('Products Table Testing:');
 
-    console.log("Testing getAllProducts...");
+    console.log('Testing getAllProducts...');
     const products = await getAllProducts();
-    console.log("Successfully tested getAllProducts:", products);
+    console.log('Successfully tested getAllProducts:', products);
 
-    console.log("Testing getProductsById using product:", products[1].id);
+    console.log('Testing getProductsById using product:', products[1].id);
     const productById = await getProductById(products[1].id);
-    console.log("Successfully tested getProdctsById:", productById);
+    console.log('Successfully tested getProdctsById:', productById);
 
-    console.log("Testing getProductByName using product:", products[2].id);
+    console.log('Testing getProductByName using product:', products[2].id);
     const productByName = await getProductByName({
-      name: "Vegan Mac N Cheese",
+      name: 'Vegan Mac N Cheese',
     });
-    console.log("Successfully tested getProductByName:", productByName);
+    console.log('Successfully tested getProductByName:', productByName);
 
-    console.log("Testing getProductsBySale...");
+    console.log('Testing getProductsBySale...');
     const productsBySale = await getProductsBySale();
-    console.log("Successfully tested getProductsBySale:", productsBySale);
+    console.log('Successfully tested getProductsBySale:', productsBySale);
 
-    console.log("Testing updateProduct using product:", products[2].id);
+    console.log('Testing updateProduct using product:', products[2].id);
     const updatedProduct = await updateProduct(products[2].id, {
-      name: "Mac Bites",
+      name: 'Mac Bites',
       description: "Grammy's deep-fried Mac N Cheese balls",
       image:
-        "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fcdn-image.myrecipes.com%2Fsites%2Fdefault%2Ffiles%2Fstyles%2Fmedium_2x%2Fpublic%2F1463689679%2Ffried-mac-and-cheese-bites-420x420.jpg%3Fitok%3DMY0nk5At",
-      inventory: "15",
-      basePrice: "10",
-      currentPrice: "5",
+        'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fcdn-image.myrecipes.com%2Fsites%2Fdefault%2Ffiles%2Fstyles%2Fmedium_2x%2Fpublic%2F1463689679%2Ffried-mac-and-cheese-bites-420x420.jpg%3Fitok%3DMY0nk5At',
+      inventory: '15',
+      basePrice: '10',
+      currentPrice: '5',
       sale: true,
-      date: "09/18/2020",
-      categories: ["bites", "cheddar ", "fried"],
+      date: '09/18/2020',
+      categories: ['bites', 'cheddar ', 'fried'],
     });
-    console.log("Successfully tested updateProduct:", updatedProduct);
+    console.log('Successfully tested updateProduct:', updatedProduct);
 
-    console.log("Testing deleteProduct using product:", products[0].name);
+    console.log('Testing deleteProduct using product:', products[0].name);
     const deletedProduct = await deleteProduct(products[0].id);
-    console.log("Successfully", deletedProduct);
+    console.log('Successfully', deletedProduct);
 
-    console.log("Products Table Testing Completed!");
+    console.log('Products Table Testing Completed!');
   } catch (error) {
-    console.log("Error running tests on products...");
+    console.log('Error running tests on products...');
     throw error;
   }
 }
--- db/products.js
+++ db/products.js
@@ -213,6 +213,14 @@ async function deleteProduct(id) {
 
     await client.query(
       `
+    DELETE FROM reviews
+    WHERE "productId"=$1;
+    `,
+      [id]
+    );
+
+    await client.query(
+      `
     DELETE FROM products
     WHERE id=$1;
     `,
