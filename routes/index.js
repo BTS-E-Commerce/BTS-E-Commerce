f5586914f401c9ec6dfd1e264adba19e6ecb3b9f
Tyler Flood <tyler.flood1@yahoo.com>
2020-09-21 17:25:55 -0400

    added Categories Component.

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,25 @@
 //~~~~~~~~~~~~~~~~~~~
 //~~~~~ IMPORTS ~~~~~
 //~~~~~~~~~~~~~~~~~~~
-const client = require('../db')
+const client = require('../db');
 const apiRouter = require('express').Router();
 
 const usersRouter = require('./users');
 const productsRouter = require('./products');
-
+const categoriesRouter = require('./categories');
 //~~~~~~~~~~~~~~~~~~~
 //~~~ MIDDLEWARE ~~~~
 //~~~~~~~~~~~~~~~~~~~
 
-apiRouter.get("/", (req, res, next) => {
+apiRouter.get('/', (req, res, next) => {
   res.send({
-    message: "API is under construction!"
+    message: 'API is under construction!',
   });
 });
 
 apiRouter.use('/users', usersRouter);
 apiRouter.use('/products', productsRouter);
+apiRouter.use('/categories', categoriesRouter);
 
 //~~~~~~~~~~~~~~~~~~~
 //~~~~~ EXPORTS ~~~~~
--- src/components/Content.js
+++ src/components/Content.js
@@ -2,34 +2,33 @@ import React, { useState, useEffect } from 'react';
 
 import ProductCard from './ProductCard';
 import NewProductForm from './NewProductForm';
-
+import CategoryList from './CategoryList';
 import { getAllProducts } from '../api/products';
 
-
 const Content = () => {
-    const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState([]);
 
-    useEffect(() => {
-        getAllProducts()
-            .then(result => {
-                setProducts(result.products);
-            })
-            .catch(error => {
-                console.log(error);
-            })
-        //Putting "products" into the change array below causes it to run forever every second. I don't know why.
-        //Nevermind, I figured it out. It was becuase I was running a console log on it right after, which counted as a "change".
-    }, [products]);
+  useEffect(() => {
+    getAllProducts()
+      .then((result) => {
+        setProducts(result.products);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    //Putting "products" into the change array below causes it to run forever every second. I don't know why.
+    //Nevermind, I figured it out. It was becuase I was running a console log on it right after, which counted as a "change".
+  }, [products]);
 
-    return (
-        <div>
-            <NewProductForm />
-            {products.map(product => (
-                <ProductCard key={product.id} product={product} />
-            ))}
-        </div>
-
-    );
-}
+  return (
+    <div>
+      <NewProductForm />
+      {products.map((product) => (
+        <ProductCard key={product.id} product={product} />
+      ))}
+      <CategoryList />
+    </div>
+  );
+};
 
 export default Content;
